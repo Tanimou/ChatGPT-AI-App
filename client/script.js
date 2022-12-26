549e41eec3880d67b7541fb218d8a89e7855f397
Tanimou cisse <tanimou@live.fr>
2022-12-26 14:31:26 +0000

    updating url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   //call the loader function
   loader(messageDiv);
   //fetch the response from the server
-  const response = await fetch('https://codex-3jvb.onrender.com', {
+  const response = await fetch('https://codex2-0wgx.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
